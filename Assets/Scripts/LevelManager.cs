8459f9b8eb84f897f0d651cafcbfc41c6d8398c4
razvanblue <razvan.razvanblue@gmail.com>
2023-07-30 17:30:19 +0300

    Add title screen and level persistent background music

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static void LoadNextLevel()
+    {
+        int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentLevelIndex + 1 < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(currentLevelIndex + 1);
+        }
+        else
+        {
+            Debug.Log("No scene left to load");
+        }
+    }
+}
